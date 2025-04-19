d731c54f75ee956402d951a53076e2433a555f9c
haya <66245708+haya-hyh@users.noreply.github.com>
2025-04-19 19:56:14 +0200

    Update mathjax.js

--- assets/js/mathjax.js
+++ assets/js/mathjax.js
@@ -1,9 +1,14 @@
+<script>
 window.MathJax = {
   tex: {
-    inlineMath: [['$', '$'], ['\\(', '\\)']],
-    displayMath: [['\\[', '\\]'], ['$$', '$$']]
+    inlineMath: [['\\(', '\\)']],
+    displayMath: [['\\[', '\\]']]
   },
   svg: {
     fontCache: 'global'
   }
 };
+</script>
+<script type="text/javascript" id="MathJax-script" async
+  src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js">
+</script>
